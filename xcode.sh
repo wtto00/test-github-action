d5d46aa8a3f6f166214fb563661600d6156436ad
简静凡 <30424139+wtto00@users.noreply.github.com>
2023-10-04 01:47:14 +0800

    2023/10/4 01:47:09

--- xcode.sh
+++ xcode.sh
@@ -4,15 +4,15 @@ xcrun simctl list
 
 xcrun simctl boot "$IPHONE_MODEL"
 
-# 等待模拟器启动完成
+echo 'wait for booted'
 while true; do
   status=$(xcrun simctl list | grep "Phone: $IPHONE_MODEL (" | awk '{print $5}')
-  echo "boot status: $status"
+  echo $(xcrun simctl list | grep "Phone: $IPHONE_MODEL (")
   if [[ "$status" == "(Booted)" ]]; then
     break
   fi
   sleep 1
 done
 
-# 在模拟器中打开链接
+echo 'openurl'
 xcrun simctl openurl booted https://google.com
