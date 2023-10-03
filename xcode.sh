c733fea64413f7a268b108aa8be17144b0c6b878
简静凡 <30424139+wtto00@users.noreply.github.com>
2023-10-04 01:50:53 +0800

    2023/10/4 01:50:48

--- xcode.sh
+++ xcode.sh
@@ -6,8 +6,8 @@ xcrun simctl boot "$IPHONE_MODEL"
 
 echo 'wait for booted'
 while true; do
-  status=$(xcrun simctl list | grep "Phone: $IPHONE_MODEL (" | awk '{print $5}')
-  echo $(xcrun simctl list | grep "Phone: $IPHONE_MODEL (")
+  status=$(xcrun simctl list | grep "$IPHONE_MODEL" | awk '{print $5}')
+  echo $(xcrun simctl list | grep "$IPHONE_MODEL")
   if [[ "$status" == "(Booted)" ]]; then
     break
   fi
